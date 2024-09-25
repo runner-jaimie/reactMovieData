b1c5699276fd8f0470b31a90ef64b150ff727fac
Jaimie Lee <jaimie@Jaimies-MacBook-Air.local>
2024-09-25 19:43:29 -0400

    Fix vite.config.js error in build process-2

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build", // build 폴더로 출력
+  },
 });
